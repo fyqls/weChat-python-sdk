f5f156eafd0232ab2a1de248927c2ac01e65b502
lisong <lisong.qiu@transwarp.io>
2016-03-27 20:46:54 +0800

    add

--- demo.py
+++ demo.py
@@ -3,16 +3,18 @@
 首先需要在微信公共平台申请帐号 mp.weixin.qq.com
 之后才可以使用下事例
 """
-__author__ = 'Vincent Ting'
+__author__ = 'lisong QIU'
 
 from weChat.client import Client
 
-client = Client(email='xx@gmail.com', password='yourPsw')
+client = Client(appid='wxfd7414fc987df416', appsecret='a7e2632b2dc9f5496520c2fc2b21e9c2')
 
 #demo  推送文字信息,其中sendto为关注该帐号的某用户的fakeId
 
-client.sendTextMsg('xxxxxxx', 'Hello world')
+#client.sendTextMsg('xxxxxxx', 'Hello world')
+
+client.do_push("","https://api.weixin.qq.com/cgi-bin/message/mass/sendall?access_token=ACCESS_TOKEN")
 
 #demo  推送图片信息,其中sendto为关注该帐号的某用户的fakeId，img为图片的文件路径
 
-client.sendImgMsg('xxxxxxx', 'E:/demo.png')
+#client.sendImgMsg('xxxxxxx', './demo.png')
--- weChat/base.py
+++ weChat/base.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime
 
 __author__ = 'Vincent Ting'
 
@@ -13,31 +14,113 @@ import re
 
 
 class BaseClient(object):
-    def __init__(self, email=None, password=None):
-        """
-        登录公共平台服务器，如果失败将报客户端登录异常错误
-        :param email:
-        :param password:
-        :raise:
-        """
-        if not email or not password:
-            raise ValueError
-        self.setOpener()
+    def __init__(self, appid=None, appsecret=None, file_name='/tmp/weixin.krb'):
 
-        url_login = "https://mp.weixin.qq.com/cgi-bin/login?lang=zh_CN"
-        m = hashlib.md5(password[0:16])
-        m.digest()
-        password = m.hexdigest()
-        body = (('username', email), ('pwd', password), ('imgcode', ''), ('f', 'json'))
+        if not appid or not appsecret:
+            raise ValueError
+
+        self.appid = appid
+        self.appsecret = appsecret
+        self.file_name = file_name
+
+    def build_timestamp(self, interval):
+        # 传入时间间隔,得到指定interval后的时间 格式为"2015-07-01 14:41:40"
+        now = datetime.datetime.now()
+        delta = datetime.timedelta(seconds=interval)
+        now_interval = now + delta
+        return now_interval.strftime('%Y-%m-%d %H:%M:%S')
+
+    def check_token_expires(self):
+        # 判断token是否过期
+        with open(self.file_name, 'r') as f:
+            line = f.read()
+            if len(line) > 0:
+                expires_time = line.split(",")[1]
+                token = line.split(",")[0]
+            else:
+                return "", "true"
+        curr_time = time.strftime('%Y-%m-%d %H:%M:%S')
+        # 如果过期返回false
+        if curr_time > expires_time:
+            return token, "false"
+        # 没过期返回true
+        else:
+            return token, "true"
+
+    def getToken(self):
+        # 获取accessToken
+        url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid=' + self.appid + "&secret=" + self.appsecret
         try:
-            msg = json.loads(self.opener.open(url_login, urllib.urlencode(body), timeout=5).read())
-        except urllib2.URLError:
-            raise ClientLoginException
-        if msg['ErrCode'] not in (0, 65202):
-            print msg
-            raise ClientLoginException
-        self.token = msg['ErrMsg'].split('=')[-1]
-        time.sleep(1)
+            f = urllib2.urlopen(url)
+            s = f.read()
+            # 读取json数据
+            j = json.loads(s)
+            j.keys()
+            # 从json中获取token
+            token = j['access_token']
+            # 从json中获取过期时长
+            expires_in = j['expires_in']
+            # 将得到的过期时长减去300秒然后与当前时间做相加计算然后写入到过期文件
+            write_expires = self.build_timestamp(int(expires_in - 300))
+            content = "%s,%s" % (token, write_expires)
+            with open(self.file_name, 'w') as f:
+                f.write(content)
+        except Exception, e:
+            print e
+        return token
+
+    def post_data(self, url, para_dct):
+        """触发post请求微信发送最终的模板消息"""
+        para_data = para_dct
+        f = urllib2.urlopen(url, para_data)
+        content = f.read()
+        return content
+
+    def do_push(self,touser,template_id,url,topcolor,data):
+        '''推送消息 '''
+        #获取存入到过期文件中的token,同时判断是否过期
+        token,if_token_expires=self.check_token_expires()
+        #如果过期了就重新获取token
+        if if_token_expires=="false":
+          token=self.getToken()
+        # 背景色设置,貌似不生效
+        if topcolor.strip()=='':
+          topcolor = "#7B68EE"
+        #最红post的求情数据
+        dict_arr = {'touser': touser, 'template_id':template_id, 'url':url, 'topcolor':topcolor,'data':data}
+        json_template = json.dumps(dict_arr)
+        requst_url = "https://api.weixin.qq.com/cgi-bin/message/template/send?access_token="+token
+        content = self.post_data(requst_url,json_template)
+        #读取json数据
+        j = json.loads(content)
+        j.keys()
+        errcode = j['errcode']
+        errmsg = j['errmsg']
+        #print errmsg
+
+    # def __init__(self, appid=None, appsecret=None, finename='/tmp/weixin.krb'):
+    #     """
+    #     登录公共平台服务器，如果失败将报客户端登录异常错误
+    #     :param email:
+    #     :param password:
+    #     :raise:
+    #     """
+    #     if not appid or not appsecret:
+    #         raise ValueError
+    #     self.setOpener()
+    #
+    #     url = 'https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid='+appid + "&secret="+appsecret
+    #     try:
+    #         f = urllib2.urlopen(url)
+    #         s = f.read()
+    #         # 读取json数据
+    #         j = json.loads(s)
+    #         j.keys()
+    #         # 从json中获取token
+    #         self.token = j['access_token']
+    #     except BaseException,e:
+    #         raise ClientLoginException(e)
+    #     time.sleep(1)
 
     def setOpener(self):
         """
@@ -53,7 +136,8 @@ class BaseClient(object):
                                   ('Host', 'mp.weixin.qq.com'),
                                   ('Origin', 'mp.weixin.qq.com'),
                                   ('X-Requested-With', 'XMLHttpRequest'),
-                                  ('User-Agent', 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36')]
+                                  ('User-Agent',
+                                   'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36')]
 
     def _sendMsg(self, sendTo, data):
         """
